8c9f43dd86a763fee8290be781d2acac62e88789
Garrett Pace <garrett_pace@stu.indianhills.edu>
2024-11-06 14:40:56 -0600

    added in the movement script

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public bool grounded = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            rigidbody2D.linearVelocityX = 1;
+        }
+
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            rigidbody2D.linearVelocityX = -1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+       if (collision.collider.CompareTag("Ground"))
+        {
+            grounded=false;
+        } 
+    }
+
+    void Jump()
+    {
+        rigidbody2D.linearVelocityY = 5;
+
+    }
+
+
+}
