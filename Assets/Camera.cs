cef8af6b77d36c06d489b05b357f433d584a2f8b
Swinglex <153555916+Swinglex@users.noreply.github.com>
2024-11-11 11:30:26 -0600

    Bosses

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float FollowSpeed = 2f;
     public float yOffset =1f;
     public Transform target;
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 newPos = new Vector3(target.position.x,target.position.y + yOffset,-10f);
